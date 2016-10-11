97f71331692a878db7c0eda24dfe37ef9b426ac1
root <vs@itsvit.org>
2016-10-11 11:49:40 -0400

    Fix

--- setup.sh
+++ setup.sh
@@ -18,21 +18,24 @@ function get_distr_famaly(){
 
 
 DISTR=`get_distr_famaly`
-echo ${DISTR}
 
 ANSIBLE=`which ansible` 2> /dev/null
 GIT=`which git` 2>/dev/null
 
 
 if [ "RedHat" == ${DISTR} ]; then
-  [ -z ${ANSIBLE} ] && yum install -y ansible
-  [ -z ${GIT} ] && yum install -y git
+  [ -z ${ANSIBLE} ] && yum install -y ansible 
+  [ -z ${GIT} ] && yum install -y git sshpass
 else 
   [ -z ${ANSIBLE} ] && apt-get install -y ansible
-  [ -z ${GIT} ] && apt-get install -y git
+  [ -z ${GIT} ] && apt-get install -y git sshpass
 fi
 
 
 cd ansible
 
-ansible-playbook -vv setup.yml -e "host=$1 root_dir=${ROOT_DIR} r_user=$2"
+if [[ -z "$3" ]]; then
+    ansible-playbook -vv setup.yml -e "host=$1 root_dir=${ROOT_DIR} r_user=$2"
+else
+    ansible-playbook -vv setup.yml -e "host=$1 root_dir=${ROOT_DIR} r_user=$2 ansible_ssh_pass=\'$3\'"
+fi
