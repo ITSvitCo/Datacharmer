b856ea4acb06b16a18d89024e7081c870cc0bdba
root <vs@itsvit.org>
2016-10-11 12:24:54 -0400

    Fix README.md

--- README.md
+++ README.md
@@ -17,12 +17,24 @@ This step-by-step guide describes how to deploy a simple PHP script that outputs
 * MySQL server 
 * Configuration Management tool for configuring server (Saltstack/Ansible/Puppet/Chef)
 
+
+# Список каталогов
+ansible - (К) плэйбуки и роли Ansible
+README.md (Ф)
+setup.sh - (Ф) Исполняемый скрипт
+site (К) - index.php 
+
+
 # Installation process
 1. Download the repository from GitHub
-
+git clone https://github.com/ITSvitCo/Datacharmer.git
+1.1. Запускаем скрипт setup.sh с параметрами 1-й адрес хоста на который будет произведен деплой, 2-й имя пользлователя для подключения к удаленному хосту, 3-й пароль (если подключение будет осущ. по ключу - пароль не нужен)
+./setup.sh 192.168.206.182 root or ./setup.sh 192.168.206.182 root 123
 ```
 command
 ```
+
+
 
 2. Change to the `directory` directory
 
@@ -36,13 +48,21 @@ command
 command <parameters>
 ```
 
+4. setup.sh
+    ROOT_DIR=/opt/itsvit-devops - директория на удаленном хосте, в которой будет хранится все необходимые файлы.
+    function get_distr_family - определяем семейство дистрибутива Linux
+    
+
 # Testing the installation
 * Navigate to the following address:
 ```
+Открываем браузер и вводим адрес удаленного хоста
+
 
 ```
 
 Once finished, you should see final results similar to those showed below:
+
 
 ```
 ```
--- setup.sh
+++ setup.sh
@@ -3,37 +3,45 @@
 #set -o xtrace
 #set -e
 
+# директория на удаленном хосте, в которой будет хранится все необходимые файлы.
 ROOT_DIR=/opt/itsvit-devops
 
-function get_distr_famaly(){
+# Определяем семейство ОС
+function get_distr_family(){
   if [ -a /etc/debian_version ]; then
     echo "Debian"
   elif [ -a /etc/redhat-release ]; then
     echo "RedHat"
   else
-    echo "This script workin only on Ubuntu, Debian or Centos OS"
+    echo "This script works only on Ubuntu, Debian or Centos OS"
     exit 2
   fi
 }
 
 
-DISTR=`get_distr_famaly`
+DISTR=`get_distr_family`
 
+# Определяем установлены ли следующие компоненты
 ANSIBLE=`which ansible` 2> /dev/null
 GIT=`which git` 2>/dev/null
+SSHPASS=`which sshpass` 2>/dev/null
 
-
+# If not install
 if [ "RedHat" == ${DISTR} ]; then
   [ -z ${ANSIBLE} ] && yum install -y ansible 
-  [ -z ${GIT} ] && yum install -y git sshpass
+  [ -z ${GIT} ] && yum install -y git
+  [ -z ${SSHPASS} ] && yum install -y sshpass
 else 
   [ -z ${ANSIBLE} ] && apt-get install -y ansible
-  [ -z ${GIT} ] && apt-get install -y git sshpass
+  [ -z ${GIT} ] && apt-get install -y git
+  [ -z ${SSHPASS} ] && apt-get install -y sshpass
+
 fi
 
-
+# Переходим в каталог с ансибл плэйбуком
 cd ansible
 
+# Проверка как нужно подключаться к хосту: По ключу или по паролю
 if [[ -z "$3" ]]; then
     ansible-playbook -vv setup.yml -e "host=$1 root_dir=${ROOT_DIR} r_user=$2"
 else
